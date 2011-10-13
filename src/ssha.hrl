8e963c53bbf4ebf47393fdc013fcfc4f664e1135
Maxim Krentovskiy <mkrentovskiy@gmail.com>
2011-10-13 13:53:55 +0400

    Add SSH agent

---
+++ src/ssha.hrl
@@ -0,0 +1,9 @@
+-define(TIMEOUT, 60000).
+-define(BANNER_TIMEOUT, 2000).
+-define(PROMPT_TIMEOUT, 1000).
+-define(COMMAND_TIMEOUT, 5000).
+
+-define(TERM_WIDTH, 800).
+-define(TERM_HEIGHT, 240).
+
+-define(LOG(F,I), io:format(F,I)).
